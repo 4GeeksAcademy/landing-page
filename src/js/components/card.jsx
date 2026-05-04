5a28f6fc57dec25402805cb16e7eaacce5995452
Miquel Arnaiz Fayos <arnaizmiquel@gmail.com>
2026-05-04 15:48:23 +0000

    vamos a ver

---
+++ src/js/components/card.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+<div className="container d-flex justify-content-center p-3 d-flex">
+<div className="card shadow-sm  p-3 p-3 col-lg-4-md-4-sm-12" >
+  <img src="https://via.placeholder.com/500x325" className="card-img-top" alt="..." />
+  <div className="card-body">
+    <h5 className="card-title text-center">Card title</h5>
+    <p className="card-text text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
+  </div>
+  <div className="card-footer text-center">
+    <a href="#" className="btn btn-primary">Find Out More!</a>
+  </div>
+</div>
+
+
+<div className="card shadow-sm  p-3 p-3 col-lg-4-md-4-sm-12" >
+  <img src="https://via.placeholder.com/500x325" className="card-img-top" alt="..." />
+  <div className="card-body">
+    <h5 className="card-title text-center">Card title</h5>
+    <p className="card-text text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
+  </div>
+  <div className="card-footer text-center">
+    <a href="#" className="btn btn-primary">Find Out More!</a>
+  </div>
+</div>
+
+
+<div className="card shadow-sm  p-3 p-3 col-lg-4-md-4-sm-12" >
+  <img src="https://via.placeholder.com/500x325" className="card-img-top" alt="..." />
+  <div className="card-body">
+    <h5 className="card-title text-center">Card title</h5>
+    <p className="card-text text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
+  </div>
+  <div className="card-footer text-center">
+    <a href="#" className="btn btn-primary">Find Out More!</a>
+  </div>
+</div>
+
+
+<div className="card shadow-sm  p-3 col-lg-4-md-4-sm-12" >
+  <img src="https://via.placeholder.com/500x325" className="card-img-top" alt="..." />
+  <div className="card-body">
+    <h5 className="card-title text-center">Card title</h5>
+    <p className="card-text text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
+  </div>
+  <div className="card-footer text-center">
+    <a href="#" className="btn btn-primary">Find Out More!</a>
+  </div>
+</div>
+</div>
+);
+}
+
+export default Card;
---
+++ src/js/components/navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark text-light">
+      <div className="container">
+        <a className="navbar-brand text-light" href="#">Start bootstrap</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+       <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav ml-auto"> 
+            <li className="nav-item active">
+              <a className="nav-link text-white" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-secondary" href="#">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-secondary" href="#">Services</a>
+            </li>
+             <li className="nav-item">
+              <a className="nav-link text-secondary" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
